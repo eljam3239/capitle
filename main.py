090f8ed18dbcc840e3d87a853c2cbdaab434e943
eljam3239 <elijames2301@gmail.com>
2022-07-11 17:26:15 -0400

    Need to figure out PD dataframes to pull first 60 valid cities

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-class guess:
-    def __init__(self, guess):
-        self.guess = str(guess)
+import answer
+import pandas as pd
+
+def is_valid_guess(guess):
+    cities = pd.read_csv("canadacities.csv", nrows = 61)
+    
+
+def guess():
+    return None
 
 if __name__ == "__main__":
-    x = guess("Hi")
-    print(x.guess)
+    test = answer.answer()
+    #print(test.test())
+    cities = pd.read_csv("canadacities.csv", nrows = 61)
+    print(cities)
